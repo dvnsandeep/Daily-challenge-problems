0c1a187133ec4004d5141eeae1a01eb9b64bc9c1
Vijaya Raghavendra <35492336+raghavendraivv@users.noreply.github.com>
2020-10-01 11:40:43 +0530

    Update 672b.c

--- 672b.c
+++ 672b.c
@@ -1,29 +1,30 @@
 #include<stdio.h>
 #include<string.h>
+#define ll long long int
+
 int main ()
-{int i,m=0,j,n,f;
-scanf("%d",&n);
-char s[n],k[n];
-scanf("%s",s);
-for(i=0;i<n;i++)
-{f=m;
-if(strlen(k)>=26)
-m++;
-for(j=0;j<strlen(k);j++)
-{ if(s[i]==k[j])
-m++;
-break;
-
-}
-if(f==m)
-{k[strlen(k)]=s[i];}
-
-}
-
-printf("%d",m);
-
-
-
+{
+   ll i,m=0,j,n,f;
+  scanf("%d",&n);
+  char s[n],k[n];
+  scanf("%s",s);
+  for(i=0;i<n;i++)
+  { 
+    f=m;
+    if(strlen(k)>=26)
+    m++;
+    for(j=0;j<strlen(k);j++)
+    { 
+      if(s[i]==k[j])
+      m++;
+      break;
+    }
+    if(f==m)
+    {
+      k[strlen(k)]=s[i];
+    }
+  }
+  printf("%d",m);
 }
 
 
